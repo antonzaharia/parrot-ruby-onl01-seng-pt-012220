ae7a0a8c199e0dd708d4497f026167ff75f7bd65
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-21 17:50:56 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(sound="Squawk!")
-  puts = "Pretty #{sound}!"
+  puts = "Pretty #{sound}"
   return "Pretty bird!"
 end
 parrot("bird")
